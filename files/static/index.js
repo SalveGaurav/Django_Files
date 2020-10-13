fd6d4a17c6f777d6ae55a5b846f93b5086d02fdf
Gaurav <gaurav.salve2204@gmail.com>
2020-10-13 11:32:48 +0530

    updated

--- files/static/index.js
+++ files/static/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#Send').click(function(){
-       /* const Ipfiles=document.getElementById("files");
+        const Ipfiles=document.getElementById("files");
         console.log(Ipfiles.files)
         const button=document.getElementById("Send");
         //const xhr=new XMLHttpRequest();
@@ -23,41 +23,7 @@ $(document).ready(function(){
               
                  alert("sucessful")
                }
-        });*/
-        var data = new FormData();
-        $.each($("#files")[0].files, function(i, file){
-            data.append("file", file);
         });
-        data.append("csrfmiddlewaretoken", $('input[name=csrfmiddlewaretoken]').val());
-        console.log(data);        
-        $.ajax({
-            type: "POST",
-            url: "/upload/",  
-            data:data,
-            processData: false,
-            contentType: false,
-            success:  function(data){
-                alert(data);
-                console.log(length.data)
-                console.log(data)
-                data = JSON.parse(data);
-                
-                
-                for(var i in data){
-                    console.log(i)
-                    var newLine=document.createElement('br');
-                    var aTag = document.createElement('a');
-                    var loc="/media/"+data[i]['file']
-                    aTag.setAttribute('href',loc);
-                    aTag.setAttribute('download',"download")
-                    var fileName=data[i]['file'];
-                    var fileName=fileName.split("/")
-                    aTag.innerText = fileName[1];
-                    $("#right").append(aTag);
-                    $("#right").append(newLine);
-                }         
-                               
-       }
-        });
+    
     });
     });
--- files/views.py
+++ files/views.py
@@ -3,21 +3,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Datas
 from django.core.files.storage import FileSystemStorage
-import pickle
-import json
 
 def index(request):
     return render(request,"index.html")
 def upload(request):
-    file=request.FILES.getlist('file')
+    file=request.FILES.getlist('uploaded_file')
     print(file)
     for f in file:
         if " " in f:
             f=changename(file)
         obj=Datas(file=f)
         obj.save()
-    files=list(Datas.objects.all().values('file'))
-    return HttpResponse(json.dumps(files))
+
+    return HttpResponse("done")
 
 def changename(file):
     oldname=file.name
